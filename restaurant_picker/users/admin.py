2c00b2bb8623296e99e0c3026cd57a17a79af7ea
MKurtz666 <mkusy936@gmail.com>
2022-03-18 21:17:08 +0100

    Changed the UserRestaurantStack into UserRestaurantPick - each pick will be saved as a separate instance of the model.

--- restaurant_picker/users/admin.py
+++ restaurant_picker/users/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, UserRestaurantStack
+from .models import User, UserRestaurantPick
 
 
-class RestaurantStackInline(admin.StackedInline):
-    model = UserRestaurantStack
-    can_delete = False
-    verbose_name_plural = 'userrestaurantstack'
-
-
-class UserAdmin(BaseUserAdmin):
-    inlines = (RestaurantStackInline,)
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(UserRestaurantStack)
+admin.site.register(User)
+admin.site.register(UserRestaurantPick)
--- restaurant_picker/users/models.py
+++ restaurant_picker/users/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
 
 
-class UserRestaurantStack(models.Model):
-    user = models.OneToOneField(User, related_name="restaurant_stack", on_delete=models.CASCADE)
-    weekly_restaurant_picks = models.ManyToManyField("restaurants.Restaurant", blank=True)
+class UserRestaurantPick(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey("restaurants.Restaurant", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+
 
